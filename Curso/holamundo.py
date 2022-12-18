b52e3ac3b3f9842115888af12878713cc273f859
MagoGaro <94136201+MagoGaro@users.noreply.github.com>
2022-12-18 00:34:38 +0000

    Initial commit

---
+++ Curso/holamundo.py
@@ -0,0 +1,14 @@
+"""
+ejecutar en la consola de Replit , de la siguiente manera 
+> python3 ./Curso/holamundo.py
+Escribimos python3 y luego la ruta del archivo a ejecutar
+
+O
+
+Podemos usar la Shell , ~/nombredetruproyecto$
+usaremos el comando cd nombredetucarpeta
+y una vez posicionados ejecutamos de esta forma:
+python3 nombredetuarchivo.py
+"""
+
+print("Hello world from ...")
---
+++ Curso/variables.py
@@ -0,0 +1,28 @@
+# Decalracion de una variable 
+
+"""Nunca debe empezar por un numero , pueden empezar por _ , es case sensitive."""
+
+name = "Gabriel"
+Name = "Sebastian"
+
+print("Hola "+name)
+print("Hola "+Name)
+
+# Variables numericas
+
+num1 = 1
+num2 = 5
+
+resultado = num1 + num2
+resultado2 = num2 - num1
+
+# encadenar
+
+"""Solo podes concantenar(+) variables de string, para mostrar numeros, se utiliza la , o la otra forma es con f{}"""
+
+#Ejemplo con ,
+print("El resultado de la suma es:",resultado)
+
+#Ejemplo con f{}
+print(f"El resultado de la resta es: {resultado2}")
+
